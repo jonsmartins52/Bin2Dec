fa066e32e894fea895aa0ef6425ac6147b0ff318
jonathan <jonathan.santosmartins@gmail.com>
2022-06-11 13:54:59 -0300

    refactor: Cor da borda com valores inválidos

--- src/view.js
+++ src/view.js
@@ -1,4 +1,5 @@
 const decEl = document.querySelector("input[name='decimal']");
+const binEl = document.querySelector("input[name='binary']");
 const spanError = document.querySelector(".convert__error");
 
 export function setDecimalInput(value) {
@@ -11,13 +12,16 @@ export function setDecimalInput(value) {
 
 export function setSpanError(value) {
   spanError.style.display = "block";
+
   spanError.innerHTML = Array.isArray(value)
     ? `${value} não são valores válidos`
     : `valor inválido`;
 
   decEl.value = "";
+  binEl.style.outline = "1px solid #DA0000";
 }
 
 export function removeSpan() {
   spanError.style.display = "none";
+  binEl.style.outline = "";
 }
